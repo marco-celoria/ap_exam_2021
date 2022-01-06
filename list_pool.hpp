22dcb5417f4d5be979a6176bd1325d1f41e79900
Marco Celoria <celoria.marco@gmail.com>
2022-01-06 20:43:20 +0100

    const iterator fixed, removed noexcept and print

--- list_pool.hpp
+++ list_pool.hpp
@@ -6,8 +6,8 @@
 
 template <typename node_t, typename T, typename N>
 class _iterator {
-    
- public:
+  
+public:
   using value_type = T;
   using list_type = N;
   using reference = value_type&;
@@ -15,200 +15,197 @@ class _iterator {
   using difference_type = std::ptrdiff_t;
   using iterator_category = std::forward_iterator_tag;
   using size_type = typename std::vector<node_t>::size_type;
-
-  _iterator(std::vector<node_t> * ptr, list_type x) noexcept : ptr_pool{ptr},current{x} {} // We do not acquire any resources
-    
-    reference operator*() const  {
-        check(current, "dereference past end");
-        return (*ptr_pool)[current-1].value;
-    }
-    
-    pointer operator->() const  { // delegate the real work to the dereference operator
-        return & this->operator*();
-    }
-    
+  
+  _iterator(std::vector<node_t> * ptr, list_type x) noexcept : ptr_pool{ptr}, current{x} {}
+  // We do not acquire any resources
+  
+  reference operator*() const  {
+    check(current, "dereference past end");
+    return (*ptr_pool)[current-1].value;
+  }
+  
+  pointer operator->() const  { // delegate the real work to the dereference operator
+    return & this->operator*();
+  }
+  
   _iterator& operator++()  {  // pre-increment
-      check(current, "increment past end");
-      current = (*ptr_pool)[current-1].next;
+    check(current, "increment past end");
+    current = (*ptr_pool)[current-1].next;
     return *this;
   }
-   
+  
   _iterator operator++(int)  {  // post-increment
     auto tmp = *this;
     ++(*this);
     return tmp;
   }
-    
+  
   friend bool operator==(const _iterator& x, const _iterator& y) noexcept {
     return x.current == y.current;
   }
-
+  
   friend bool operator!=(const _iterator& x, const _iterator& y) noexcept {
     return !(x == y);
   }
   
 private:
-    std::vector<node_t> * ptr_pool ;
-    N current;
-    
-    void check(list_type i, const std::string &msg) const {
+  std::vector<node_t> * ptr_pool ;
+  N current;
+  
+  void check(list_type i, const std::string &msg) const {
       if (size_type(i) < 1 || size_type(i) > ptr_pool->size())
-          throw std::out_of_range(msg);
-    }
+	throw std::out_of_range(msg);
+  }
 };
 
 template <typename T, typename N = std::size_t>
 class list_pool {
-    
-    struct node_t {
-      node_t() noexcept = default;
-      node_t(const T v, const N n) noexcept : value{v}, next{n} {}
-      node_t(const node_t& x) noexcept = default;            // We do not acquire any resources
-      node_t& operator=(const node_t& x) noexcept = default; // We do not acquire any resources
-      node_t(node_t&& x) noexcept = default;
-      node_t& operator=(node_t&&) noexcept = default;
-      ~node_t() = default; // noexcept by default, right?
-      T value;
-      N next;
+  
+  struct node_t {
+    node_t() noexcept = default;
+    node_t(const T v, const N n) : value{v}, next{n} {}
+    // Question: When I move/move-assign nodes, I am moving also the T value object.
+    // What if the T object is complicated, and it has a move constructor that throws an error?
+    // Maybe it is better to mark it as potentially-throwing...
+    T value;
+    N next;
     };
-
-    using list_type = N;
-    using value_type = T;
-    using size_type = typename std::vector<node_t>::size_type;
-    
-    std::vector<node_t> pool;
-    list_type free_node_list{0}; // at the beginning, it is empty
   
-    node_t& node(list_type x) noexcept { return pool[x-1]; }
-    const node_t& node(list_type x) const noexcept { return pool[x-1]; }
-
-    template <typename X>
-    list_type _push_front(X&& val, list_type head)
-    {
-        if (free_node_list) {
-            auto new_head = free_node_list;
-            free_node_list=next(free_node_list);
-            value(new_head)=val;
-            if (is_empty(head)) next(new_head)=end();
-            else next(new_head)=head;
-            return new_head;
-        }
-        else {
-            if (is_empty(head)) pool.emplace_back(val, end());
-            else pool.emplace_back(val, head);
-            return pool.size();
-        }
-    }
+  using list_type = N;
+  using value_type = T;
+  using size_type = typename std::vector<node_t>::size_type;
     
-    template <typename X>
-    list_type _push_back(X&& val, list_type head) {
-        if (is_empty(head)) return push_front(val, head);
+  std::vector<node_t> pool;
+  list_type free_node_list{0}; // at the beginning, it is empty
+  
+  node_t& node(list_type x) noexcept { return pool[x-1]; }
+  const node_t& node(list_type x) const noexcept { return pool[x-1]; }
+  
+  template <typename X>
+  list_type _push_front(X&& val, list_type head)
+  {
+    if (free_node_list) {
+      auto new_head = free_node_list;
+      free_node_list=next(free_node_list);
+      value(new_head)=val;
+      if (is_empty(head)) next(new_head)=end();
+      else next(new_head)=head;
+      return new_head;
+    }
+    else {
+      if (is_empty(head)) pool.emplace_back(val, end());
+      else pool.emplace_back(val, head);
+      return pool.size();
+    }
+  }
+  
+  template <typename X>
+  list_type _push_back(X&& val, list_type head) {
+    if (is_empty(head)) return push_front(val, head);
         // find last node
-        list_type it = head;
-        while(next(it)!= end()) it=next(it);
-        if (free_node_list) {
-            auto new_tail = free_node_list;
-            free_node_list=next(free_node_list);
-            value(new_tail)=val;
-            next(new_tail)=end();
-            next(it)=new_tail;
-        }
-        else {
-            pool.emplace_back(val, end());
-            next(it)=pool.size();
-        }
-        return head;
+    list_type it = head;
+    while(next(it)!= end()) it=next(it);
+    if (free_node_list) {
+      auto new_tail = free_node_list;
+      free_node_list=next(free_node_list);
+      value(new_tail)=val;
+      next(new_tail)=end();
+      next(it)=new_tail;
     }
-    
-    void _print(list_type it) const noexcept {
-        while(it!= end()) {
-            std::cout <<value(it) << " " ;
-            it=next(it);
-        }
-        std::cout << std::endl;
+    else {
+      pool.emplace_back(val, end());
+      next(it)=pool.size();
     }
+    return head;
+  }
 
-    
-  public:
-    
-    list_pool() noexcept = default ;
-    explicit list_pool(size_type n) {  pool.reserve(n); } // reserve n nodes in the pool
-    list_pool(const list_pool& x) = default;
-    list_pool& operator=(const list_pool& x) = default;
-    list_pool(list_pool&& x) noexcept = default ;
-    list_pool& operator=(list_pool&& x) noexcept = default;
-    ~list_pool() = default; // noexcept by default, right?
-
-    using iterator =  _iterator<node_t, T, N>;
-    using const_iterator =  _iterator<node_t, const T, const N>;
-
-    iterator begin(list_type head) noexcept { return iterator{ &pool, head }; }
-    iterator end(list_type ) noexcept { return iterator{ &pool, list_type (0) }; } // this is not a typo
-    
-    const_iterator begin(list_type head) const noexcept { return const_iterator{ &pool, head }; }
-    const_iterator end(list_type ) const noexcept { return const_iterator{ &pool, list_type(0) };  }
+  /*
+  void _print(list_type it) const noexcept {  // This makes sense only if value is ``printable''
+    while(it!= end()) {
+      std::cout << value(it) << " " ;         // Maybe it is better to comment this function
+      it=next(it);                            // We cannot std::cout << std::vector(..) ..
+    }
+    std::cout << std::endl;
+  }
+  */
   
-    const_iterator cbegin(list_type head) const noexcept { return const_iterator{ &pool, head }; }
-    const_iterator cend(list_type ) const noexcept { return const_iterator{ &pool, list_type(0) }; }
-    
-    list_type new_list()  { reserve(8); return list_type(0); } // return an empty list
-
-    void reserve(size_type n) { pool.reserve(n); } // reserve n nodes in the pool
-    
-    size_type capacity() const noexcept { return pool.capacity(); } // the capacity of the pool
-
-    bool is_empty(list_type x) const noexcept { return x==end(); }
-
-    list_type end() const noexcept { return list_type(0); }
-
-    T& value(list_type x) noexcept { return node(x).value; }
-    
-    const T& value(list_type x) const noexcept { return node(x).value; }
-
-    list_type& next(list_type x) noexcept { return node(x).next; }
-    
-    const list_type& next(list_type x) const noexcept { return node(x).next; }
-
-    list_type push_front(const T& val, list_type head) { return _push_front(val,head); }
-    list_type push_front(T&& val, list_type head)      { return _push_front(std::move(val), head); }
-    list_type push_back(const T& val, list_type head)  { return _push_back(val,head); }
-    list_type push_back(T&& val, list_type head)       { return _push_back(std::move(val), head); }
+public:
   
-    list_type free(list_type x) noexcept { // delete first node
-        if(is_empty(x)) { std::cout << "The list is already empty" << std::endl; return x;}
-        auto tmp = next(x);
-        next(x) = free_node_list;
-        free_node_list = x;
-        return tmp;
+  list_pool() noexcept = default ;
+  explicit list_pool(size_type n) {  pool.reserve(n); } // reserve n nodes in the pool
+  
+  using iterator =  _iterator<node_t, T, N>;
+  using const_iterator =  _iterator<node_t, const T,  N>;
+  
+  iterator begin(list_type head) noexcept { return iterator{ &pool, head }; }
+  iterator end(list_type ) noexcept { return iterator{ &pool, list_type (0) }; } // this is not a typo
+  
+  const_iterator begin(list_type head) const noexcept { return const_iterator{ &pool, head }; }
+  const_iterator end(list_type ) const noexcept { return const_iterator{ &pool, list_type(0) };  }
+  
+  const_iterator cbegin(list_type head) const noexcept { return const_iterator{ const_cast<std::vector<node_t> *>(&pool), head }; }
+  const_iterator cend(list_type ) const noexcept { return const_iterator{const_cast<std::vector<node_t> *>(&pool), list_type(0) }; }
+  
+  list_type new_list()  { reserve(8); return list_type(0); } // return an empty list
+  
+  void reserve(size_type n) { pool.reserve(n); } // reserve n nodes in the pool
+  
+  size_type capacity() const noexcept { return pool.capacity(); } // the capacity of the pool
+  
+  bool is_empty(list_type x) const noexcept { return x==end(); }
+  
+  list_type end() const noexcept { return list_type(0); }
+  
+  T& value(list_type x) noexcept { return node(x).value; }
+  
+  const T& value(list_type x) const noexcept { return node(x).value; }
+  
+  list_type& next(list_type x) noexcept { return node(x).next; }
+  
+  const list_type& next(list_type x) const noexcept { return node(x).next; }
+  
+  list_type push_front(const T& val, list_type head) { return _push_front(val,head); }
+  list_type push_front(T&& val, list_type head)      { return _push_front(std::move(val), head); }
+  list_type push_back(const T& val, list_type head)  { return _push_back(val,head); }
+  list_type push_back(T&& val, list_type head)       { return _push_back(std::move(val), head); }
+  
+  list_type free(list_type x) noexcept { // delete first node
+    if(is_empty(x)) { std::cout << "The list is already empty" << std::endl; return x;}
+    auto tmp = next(x);
+    next(x) = free_node_list;
+    free_node_list = x;
+    return tmp;
+  }
+  
+  list_type free_list(list_type x) noexcept { // free entire list
+    if(is_empty(x)) { std::cout <<"The list is already empty" << std::endl; return x; }
+    while(x!= end())
+      x=free(x);
+    return x;
+  }
+  
+  size_type list_size(list_type head) const noexcept {
+    size_type cnt = 0;
+    while(head!= end()) {
+      ++cnt;
+      head=next(head);
     }
-
-    list_type free_list(list_type x) noexcept { // free entire list
-        if(is_empty(x)) { std::cout <<"The list is already empty" << std::endl; return x; }
-        while(x!= end())
-            x=free(x);
-        return x;
-    }
-    
-    size_type list_size(list_type head) const noexcept {
-        size_type cnt = 0;
-        while(head!= end()) {
-            ++cnt;
-            head=next(head);
-        }
-        return cnt;
-    }
-    
-    void print_list(const list_type head) const noexcept {
-        _print(head);
-    }
-    
-    void print_free_list() const noexcept {
-        _print(free_node_list);
-    }
-    
-    void print_pool() const noexcept {
-        if(pool.size()==0) { std::cout << "pool is empty" << std::endl; return; }
-        for(size_type i = 0; i < pool.size() ; ++i)
-            std::cout << "(pool[" << i << "].value = " << pool[i].value << ", pool[" << i << "].next= " << pool[i].next << " )" << std::endl;
-    }
+    return cnt;
+  }
+  /*
+  void print_list(const list_type head) const noexcept {
+    _print(head);
+  }
+  
+  void print_free_list() const noexcept {
+    _print(free_node_list);
+  }
+  
+  void print_pool() const noexcept {
+    if(pool.size()==0) { std::cout << "pool is empty" << std::endl; return; }
+    for(size_type i = 0; i < pool.size() ; ++i)
+      std::cout << "(pool[" << i << "].value = " << pool[i].value
+		<< ", pool[" << i << "].next= " << pool[i].next << " )" << std::endl;
+  }
+  */
 };
--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 #include <cassert>
 #include <algorithm>
 
+class foo
+{
+  friend std::ostream &operator<<(std::ostream &os, const foo &item) {
+    os << item.msg;
+    return os;
+  }
+  
+public:
+  foo() = default;
+  foo(const std::string & message) : msg{message} {}
+  std::string msg;
+};
+
 int main()
 {
   list_pool<int> pool{};
@@ -27,33 +40,17 @@ int main()
   l1 = pool.push_front(2, l1);
   l1 = pool.push_front(1, l1);
   
-auto M = std::max_element(pool.begin(l1), pool.end(l1));
-assert(*M == 11);
+  auto M = std::max_element(pool.begin(l1), pool.end(l1));
+  assert(*M == 11);
 
-auto m = std::min_element(pool.begin(l1), pool.end(l1));
-assert(*m == 1);
+  auto m = std::min_element(pool.cbegin(l1), pool.cend(l1));
+  assert(*m == 1);
   for(auto it =pool.begin(l1); it !=  pool.end(l1); ++it )
     {
       std::cout << "*it= " << *it << std::endl;
       ++cnt;
     }
-  std::cout << "cnt= " << cnt << std::endl;
-  std::cout << "l1: ";
-  pool.print_list(l1);
-  std::cout << "free list: ";
-  pool.print_free_list();
-  std::cout << "all elements list: \n";
-  pool.print_pool();
-  l1=pool.free_list(l1);
-  std::cout << std::endl;
-  std::cout << "l1: ";
-  pool.print_list(l1);
-  std::cout << "free list: ";
-  pool.print_free_list();
-  std::cout << "all elements list: \n";
-  pool.print_pool();
-  // assert(*M == 9);
- 
+  l1=pool.free_list(l1);  
   l1 = pool.push_front(11, l1);
   l1 = pool.push_front(10, l1);
   l1 = pool.push_front(9, l1);
@@ -65,85 +62,39 @@ assert(*m == 1);
   l1 = pool.push_front(3, l1);
   l1 = pool.push_front(2, l1);
   l1 = pool.push_front(1, l1);
-  pool.print_list(l1);
+  
   std::cout << "++++++++++++++++" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "1---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "2---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "3---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "4---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "5---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "6---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "7---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "8---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "9---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "10---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "11---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "12---------------" << std::endl;
   l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "13---------------" << std::endl;
-  l1 = pool.free(l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "--------------------" << std::endl;  
-  pool.print_free_list();
   std::cout << "++++++++++++++++" << std::endl;
+  l1 = pool.push_front(3, l1);
   l1 = pool.push_front(2, l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-  std::cout << "++++++++++++++++" << std::endl;
   l1 = pool.push_front(1, l1);
-  pool.print_list(l1);
-  pool.print_free_list();
-    
+  for(auto it = pool.begin(l1); it !=  pool.end(l1); ++it )
+    std::cout << *it << " ";
+  std::cout << std::endl;
+  auto N = std::find(pool.cbegin(l1), pool.cend(l1), 2);
+  if(N!=pool.cend(l1)) std::cout << "This is not the end, my friend: " << *N << std::endl;
+  std::cout << std::endl;
   std::cout << "=============================" << std::endl;
-
+  
   list_pool<int> pool2{22};
-
+  
   auto l2 = pool2.new_list();
   std::cout << "l2: ";
-  pool2.print_list(l2);
   std::cout << "free list: ";
-  pool2.print_free_list();
   std::cout << "all elements list: \n";
-  pool2.print_pool();
   l2 = pool2.push_front(11, l2);
   l2 = pool2.push_front(10, l2);
   l2 = pool2.push_front(9, l2);
@@ -155,14 +106,11 @@ assert(*m == 1);
   l2 = pool2.push_front(3, l2);
   l2 = pool2.push_front(2, l2);
   l2 = pool2.push_front(1, l2);
-  pool2.print_list(l2);
   std::cout << "--------------------" << std::endl;
   l2=pool2.free_list(l2);
-  pool2.print_list(l2);
-  pool2.print_free_list();
-   
+  
   std::cout << "!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
-     
+  
   list_pool<int> pool3{};
   auto l3 = pool3.new_list();
   l3 = pool3.push_back(1, l3);
@@ -176,73 +124,39 @@ assert(*m == 1);
   l3 = pool3.push_back(9, l3);
   l3 = pool3.push_back(10, l3);
   l3 = pool3.push_back(11, l3);
-  pool3.print_list(l3);
   std::cout << "++++++++++++++++" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "1---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "2---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "3---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "4---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "5---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "6---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "7---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "8---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "9---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "10---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "11---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "12---------------" << std::endl;
   l3 = pool3.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "13---------------" << std::endl;
   l3 = pool.free(l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "--------------------" << std::endl;  
-  pool3.print_free_list();
   std::cout << "++++++++++++++++" << std::endl;
   l3 = pool3.push_front(1, l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "++++++++++++++++" << std::endl;
   l3 = pool3.push_back(2, l3);
-  pool3.print_list(l3);
-  pool3.print_free_list();
   std::cout << "=============================" << std::endl;
   
   list_pool<int> pool4{22};
@@ -260,17 +174,11 @@ assert(*m == 1);
   l4 = pool4.push_back(9, l4);
   l4 = pool4.push_back(10, l4);
   l4 = pool4.push_back(11, l4);
-  pool4.print_list(l4);
   std::cout << "--------------------" << std::endl;
-  l4=pool4.free_list(l4);
-  pool4.print_list(l4);
-  pool4.print_free_list(); 
   
   list_pool<int> pool10{};
   auto l10 = pool10.new_list();
-  auto l11 = pool10.new_list();
-  pool10.print_list(l10);
-  pool10.print_list(l11);  
+  auto l11 = pool10.new_list();  
   l10 = pool10.push_front(11, l10);
   l10 = pool10.push_front(10, l10);
   l10 = pool10.push_front(9, l10);
@@ -283,13 +191,56 @@ assert(*m == 1);
   l10 = pool10.push_front(2, l10);
   l10 = pool10.push_front(1, l10);
   l11 = pool10.push_front(100, l11);
-  pool10.print_list(l10);
-  pool10.print_list(l11);
-  l10=pool10.free_list(l10);
-  pool10.print_list(l10);
-  pool10.print_list(l11);
-  l11=pool10.free_list(l11);
-  pool10.print_list(l10);
-  pool10.print_list(l11);
+
+  std::cout << "copy" << std::endl;
+  list_pool<int> pool_copy{pool10};
+  
+  std::cout << "assignment" << std::endl;
+  pool_copy=pool4;
+  
+  std::cout << "move" << std::endl;
+  list_pool<int> pool_move{std::move(pool10)};
+  std::cout << "move assignment" << std::endl;
+  l4=pool4.free_list(l4);
+  l4=pool4.push_back(8,l4);
+  pool_move=std::move(pool4);
+
+  std::cout << "reserve" << std::endl;
+  list_pool<foo> pool_reserve{5};
+  std::cout <<"capacity="<< pool_reserve.capacity() << std::endl;
+  auto lres = pool_reserve.new_list();
+  std::cout <<"capacity="<< pool_reserve.capacity() << std::endl;
+  foo msg1{"I've just had my third dose of vaccine."};
+  foo msg2{"What I write is..."};
+  foo msg3{"unpredictable."};
+  lres=pool_reserve.push_back(msg1,lres);
+  lres=pool_reserve.push_back(msg2,lres);
+  lres=pool_reserve.push_back(msg3,lres);
+  std::cout <<"size="<<  pool_reserve.list_size(lres) << std::endl;
+  for(auto it = pool_reserve.begin(lres); it !=  pool_reserve.end(lres); ++it )
+    {
+      std::cout << it->msg << " ";
+    }
+  std::cout << std::endl;
+  lres=pool_reserve.free_list(lres);
+  lres=pool_reserve.push_back(foo{"1"},lres);
+  lres=pool_reserve.push_back(foo{"2"},lres);
+  lres=pool_reserve.push_back(foo{"3"},lres);
+  lres=pool_reserve.push_back(foo{"4"},lres);
+  std::cout <<"size="<<  pool_reserve.list_size(lres) << std::endl;
+  std::cout << "Throw check:" << std::endl;
+  std::cout << "This shall NOT throw error:" << std::endl;
+  for(auto it = pool_reserve.begin(lres); it !=  pool_reserve.end(lres);  )
+    {
+      std::cout << (it++)->msg << " "<< std::endl;
+    }
+  std::cout << std::endl;
+  std::cout << "This shall throw error:" << std::endl;
+  std::cout <<"*end="<< *(pool_reserve.end(lres)) << std::endl;
+  // for(auto it = pool_reserve.begin(lres); it !=  pool_reserve.end(lres);  )
+  //  {
+  //    std::cout << (++it)->msg << " "<< std::endl;
+  //  }
+  std::cout << std::endl;
   return 0;
 }
